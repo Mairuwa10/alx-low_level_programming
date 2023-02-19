48cba1cccadf455240c6099898dac8f064a03ac2
Mairuwa Tony <mairuwatony10@gmail.com>
2023-02-19 14:55:06 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints the size of various computer types
- * Return: 0 (Sucess)
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int %lu byte(s)\n", sizeof(int));
-printf("Size of a long int %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int %lu byte(s)\n", sizeof(long long int));
-printf("Size of a float %lu byte(s)\n", sizeof(float));
-return (0);
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
 }
